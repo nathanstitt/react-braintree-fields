f71a0d8cea9994f9fbbe9e180da5b55248cdf37f
Nathan Stitt <nathan@stitt.org>
2017-04-08 18:29:31 -0500

    fix demo; display results of tokenization

--- demo.jsx
+++ demo.jsx
@@ -8,51 +8,75 @@ class BraintreeHostedfieldDemo extends React.PureComponent {
 
     constructor(props) {
         super(props);
-        ['onCardTypeChange', 'getToken'].forEach((prop) =>
-            this[prop] = this[prop].bind(this)
-        );
+        [
+            'onError',
+            'getToken',
+            'onCardTypeChange',
+        ].forEach(prop => (this[prop] = this[prop].bind(this)));
+    }
+
+    state = {}
+
+    onError(error) {
+        this.setState({ error });
     }
 
     getToken() {
-        this.tokenize().then((t) => {
-            debugger
-        })
+        this.tokenize().then(
+            token => this.setState({ token, error: null }),
+        ).catch(
+            error => this.setState({ token: null, error }),
+        );
     }
 
     onCardTypeChange({ cards }) {
-        this.setState({ card: (cards.length == 1) ? cards[0].type : '' });
+        this.setState({ card: (1 === cards.length) ? cards[0].type : '' });
     }
 
     state = {
         numberFocused: false,
     }
 
+    renderResult(title, obj) {
+        if (!obj) { return null; }
+        return (
+            <div>
+                <b>{title}:</b>
+                <pre>{JSON.stringify(obj, null, 4)}</pre>
+            </div>
+        );
+    }
+
     render() {
         return (
             <div>
                 <h1>Braintree Hosted Fields Demo</h1>
+                {this.renderResult('Error', this.state.error)}
+                {this.renderResult('Token', this.state.token)}
+
                 <Braintree
                     authorization='sandbox_g42y39zw_348pk9cgf3bgyw2b'
-                    onError={console.log}
+                    onError={this.onError}
                     getTokenRef={t => (this.tokenize = t)}
                     onCardTypeChange={this.onCardTypeChange}
                     styles={{
-                        'input': {
+                        input: {
                             'font-size': '14px',
                             'font-family': 'helvetica, tahoma, calibri, sans-serif',
-                            'color': '#7d6b6b',
+                            color: '#7d6b6b',
                         },
                         ':focus': {
-                            'color': 'black',
+                            color: 'black',
                         },
                     }}
                 >
                     <div>
                         Number:
-                        <Field type="number"
-                               onBlur={() => this.setState({ numberFocused: false })}
-                               onFocus={() => this.setState({ numberFocused: true })}
-                               className={this.state.numberFocused ? 'focused' : ''}
+                        <Field
+                            type="number"
+                            onBlur={() => this.setState({ numberFocused: false })}
+                            onFocus={() => this.setState({ numberFocused: true })}
+                            className={this.state.numberFocused ? 'focused' : ''}
                         />
                         <p>Card type: {this.state.card}</p>
                         Date:
@@ -71,7 +95,7 @@ class BraintreeHostedfieldDemo extends React.PureComponent {
                     <button onClick={this.getToken}>Get token</button>
                 </div>
             </div>
-        )
+        );
     }
 }
 
