5a4fc86a9ff2a3744d9d386c725e06650092aade
Nathan Stitt <nathan@stitt.org>
2017-04-08 12:32:53 -0500

    add a few handlers

--- demo.jsx
+++ demo.jsx
@@ -3,41 +3,68 @@ import React from 'react';
 
 import { Braintree, Field } from './src/index';
 
-function onFocus(ev) {
 
-}
-render((
-    <div>
-        <h1>Braintree Hosted Fields Demo</h1>
-        <Braintree
-            authorization='sandbox_g42y39zw_348pk9cgf3bgyw2b'
-            onError={console.log}
-            styles={{
-                'input': {
-                    'font-size': '14px',
-                    'font-family': 'helvetica, tahoma, calibri, sans-serif',
-                    'color': '#3a3a3a'
-                },
-                ':focus': {
-                    'color': 'black'
-                }
-            }}
-        >
+class BraintreeHostedfieldDemo extends React.PureComponent {
+
+    constructor(props) {
+        super(props);
+        ['onCardTypeChange'].forEach((prop) =>
+            this[prop] = this[prop].bind(this)
+        );
+    }
+
+    onCardTypeChange({ cards }) {
+        this.setState({ card: (cards.length == 1) ? cards[0].type : '' });
+    }
+
+    state = {
+        numberFocused: false,
+    }
+
+    render() {
+        return (
             <div>
-                <h3>child</h3>
-                Number:
-                <Field type="number" onFocus={onFocus} />
-                Date:
-                <Field type="expirationDate" />
-                Month:
-                <Field type="expirationMonth" />
-                Year:
-                <Field type="expirationYear" />
-                CVV:
-                <Field type="cvv" />
-                Zip:
-                <Field type="postalCode" />
+                <h1>Braintree Hosted Fields Demo</h1>
+                <Braintree
+                    authorization='sandbox_g42y39zw_348pk9cgf3bgyw2b'
+                    onError={console.log}
+                    onCardTypeChange={this.onCardTypeChange}
+                    styles={{
+                        'input': {
+                            'font-size': '14px',
+                            'font-family': 'helvetica, tahoma, calibri, sans-serif',
+                            'color': '#7d6b6b',
+                        },
+                        ':focus': {
+                            'color': 'black',
+                        },
+                    }}
+                >
+                    <div>
+                        Number:
+                        <Field type="number"
+                               onBlur={() => this.setState({ numberFocused: false })}
+                               onFocus={() => this.setState({ numberFocused: true })}
+                               className={this.state.numberFocused ? 'focused' : ''}
+                        />
+                        <p>Card type: {this.state.card}</p>
+                        Date:
+                        <Field type="expirationDate" />
+                        Month:
+                        <Field type="expirationMonth" />
+                        Year:
+                        <Field type="expirationYear" />
+                        CVV:
+                        <Field type="cvv" />
+                        Zip:
+                        <Field type="postalCode" />
+                    </div>
+                </Braintree>
             </div>
-        </Braintree>
-    </div>
+        )
+    }
+}
+
+render((
+    <BraintreeHostedfieldDemo />
 ), document.getElementById('root'));
