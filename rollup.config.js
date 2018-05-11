cedc1a96afb67f8220c2784cb9ee745cdfcb8283
Nathan Stitt <nathan@stitt.org>
2018-05-11 15:18:10 -0500

    Update demo files for ghpages

--- rollup.config.js
+++ rollup.config.js
@@ -2,38 +2,53 @@ import babel from 'rollup-plugin-babel';
 
 const pkg = require('./package.json');
 
-export default {
-    entry: 'src/index.js',
-    plugins: [
-        babel({
-            babelrc: true,
-            exclude: 'node_modules/**',
-            presets: [['es2015', { modules: false }], 'react', 'es2015-rollup'],
-            plugins: [
-                'transform-decorators-legacy',
-                'transform-class-properties',
-                'transform-object-rest-spread',
-            ],
-        }),
-    ],
-    globals: {
-        react: 'React',
-        invariant: 'invariant',
-        'prop-types': 'PropTypes',
-        'braintree-web/client': 'Braintree',
-        'braintree-web/hosted-fields': 'HostedFields',
-    },
-    external: ['react', 'prop-types', 'braintree-web/client', 'braintree-web/hosted-fields'],
-    targets: [
-        {
-            dest: pkg.main,
+const external = ['react', 'prop-types', 'braintree-web/data-collector', 'braintree-web/client', 'braintree-web/hosted-fields'];
+
+const plugins = [
+    babel({
+        babelrc: true,
+        exclude: 'node_modules/**',
+        presets: [['es2015', { modules: false }], 'react', 'es2015-rollup'],
+        plugins: [
+            'transform-decorators-legacy',
+            'transform-class-properties',
+            'transform-object-rest-spread',
+        ],
+    }),
+];
+
+const globals = {
+    react: 'React',
+    invariant: 'invariant',
+    'prop-types': 'PropTypes',
+    'braintree-web/client': 'Braintree',
+    'braintree-web/hosted-fields': 'BraintreeHostedFields',
+    'braintree-web/data-collector': 'BraintreeDataCollector',
+};
+
+const input = 'src/index.js';
+
+export default [
+    {
+        input,
+        plugins,
+        external,
+        output: {
             format: 'umd',
             name: 'react-braintree-fields',
             sourceMap: true,
-        }, {
-            dest: pkg.module,
+            file: pkg.browser,
+            globals,
+        },
+    }, {
+        input,
+        plugins,
+        external,
+        output: {
             format: 'es',
             sourceMap: true,
+            file: pkg.module,
+            globals,
         },
-    ],
-};
+    },
+];
